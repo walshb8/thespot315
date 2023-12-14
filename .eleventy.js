03223f4a4ba65371389368330f46bcdca54b6ab0
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2023-12-14 17:06:43 +0000

    testing exposing deployURL

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,7 @@ module.exports = function(eleventyConfig) {
     console.log(process.env.FULL_URL)
     eleventyConfig.addPassthroughCopy("src/assets");
     eleventyConfig.addWatchTarget("./src/");
-  
+    eleventyConfig.addGlobalData('deployURL',process.env.FULL_URL)
     
     // Return your Object options:
     return {
