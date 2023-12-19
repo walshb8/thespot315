5dfa5629a0ff5c88f1a2e1de2942bcc2879e67e0
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2023-12-19 18:20:38 -0500

    updates build step and 11ty config

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,23 @@
+const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
+const urlBase=process.env.BASE_URL || ""
+
 module.exports = function(eleventyConfig) {
-    console.log(process.env.FULL_URL)
+  console.log(process.env.NODE_ENV);
+  let p = {}
+  if (process.env.NODE_ENV === 'build') {
+    console.log('running in production');
+    eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
+    // this should be set 
+    p={ pathPrefix: urlBase}
+  }
+  
     eleventyConfig.addPassthroughCopy("src/assets");
     eleventyConfig.addWatchTarget("./src/");
-    eleventyConfig.addGlobalData('deployURL',process.env.FULL_URL)
+  
     
     // Return your Object options:
     return {
+     ...p,
       dir: {
         input: "src",
         output: "docs",
