2e9f6d58a564712de95dbccf2ee896dedf47dd12
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2024-03-14 17:04:04 -0400

    assorted a11y tweaks on nav

--- .eleventy.js
+++ .eleventy.js
@@ -1,27 +1,26 @@
 const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
-const urlBase=process.env.BASE_URL || ""
+const urlBase = process.env.BASE_URL || "";
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   console.log(process.env.NODE_ENV);
-  let p = {}
-  if (process.env.NODE_ENV === 'build') {
-    console.log('running in production');
+  let p = {};
+  if (process.env.NODE_ENV === "build") {
+    console.log("running in production");
     eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
-    // this should be set 
-    p={ pathPrefix: urlBase}
+    // this should be set
+    p = { pathPrefix: urlBase };
   }
-  
-    eleventyConfig.addPassthroughCopy("src/assets");
-    eleventyConfig.addWatchTarget("./src/");
-  
-    
-    // Return your Object options:
-    return {
-     ...p,
-      dir: {
-        input: "src",
-        output: "docs",
-        includes: "_includes",
-      }
-    }
+
+  eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addWatchTarget("./src/");
+
+  // Return your Object options:
+  return {
+    ...p,
+    dir: {
+      input: "src",
+      output: "docs",
+      includes: "_includes",
+    },
   };
+};
